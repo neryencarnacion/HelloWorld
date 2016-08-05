ac4a86145ee1e849971c22d4ed295fdaebd547c4
neryencarnacion <padro82@gmail.com>
2016-08-05 16:07:49 -0500

    initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+
+import java.util.Date;
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date dteToday = new Date();
+		System.out.println("Hello world! @ " + dteToday.toString());
+	}
+
+}
